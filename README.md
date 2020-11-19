3c542d037154d7ae44653ceb37a9543ce4226a74
abinm670 <abinm670@mtroyal.ca>
2020-11-19 09:20:16 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Data Warehousing Project3
+Sparkify start up company has log file for customer activity, that is setting in Amazon cloud storage S3. 
+
+## Objectives 
+
+# Refrence:
+
+### How to convert epoch to datetime redshift?
+https://stackoverflow.com/questions/39815425/how-to-convert-epoch-to-datetime-redshift
+### Redshift DISTKEY / SORTKEY
+https://stackoverflow.com/questions/52885257/redshift-distkey-sortkey
+
+## data 
+http://udacity-dend.s3.amazonaws.com/song-data/A/B/N/TRABNQK128F14A2178.json
---
+++ etl.py
@@ -0,0 +1,46 @@
+import configparser
+import psycopg2
+from sql_queries import copy_table_queries , insert_table_queries
+
+ 
+def load_staging_tables(cur, conn):
+    # for query in copy_table_queries:
+        try:
+            cur.execute(copy_table_queries)
+            records=cur.fitchall()
+            for i in records :
+                print(i, "yes")
+            conn.commit()
+        except psycopg2.Error as err:
+            print("no", err)
+            conn.rollback()
+
+
+def insert_tables(cur, conn):
+    for query in insert_table_queries:
+        try:
+            cur.execute()
+            conn.commit()
+        except psycopg2.Error as errorMsg:
+            print(errorMsg, "instert issue")
+            conn.rollback()
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn = psycopg2.connect("host={} dbname={} user={} \
+        password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+    
+    load_staging_tables(cur, conn)
+    #insert_tables(cur, conn)
+
+    conn.close()
+    
+
+
+
+if __name__ == "__main__":
+    main()
