fd15841ac5dc90d4f7df488109f3b2ab15acc014
Abdullah Binmahfouz <abinm670@mtroyal.ca>
2020-11-20 08:08:43 +0300

    Add Docstrings to create_tables.py and  etl.py

--- create_tables.py
+++ create_tables.py
@@ -2,11 +2,18 @@ import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
+''' Loop inside the drop_table_queries list and execute each index
+drop tables '''
+
 
 def drop_tables(cur, conn):
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
+
+
+''' Loop inside the create_table_queries list and execute each index 
+create tables'''
 
 
 def create_tables(cur, conn):
@@ -15,8 +22,10 @@ def create_tables(cur, conn):
         conn.commit()
 
 
-''' The main function will log to the star schema in the AWS Redshift \
-     '''
+'''The main function will log to the star schema on Redshift(AWS) \
+    two functions will be called within two arguments:
+        * 1- connection to the database using the VPC
+        * 2- cursor to execute the SQL query '''
 
 
 def main():
--- etl.py
+++ etl.py
@@ -2,6 +2,9 @@ import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
+''' Loop inside the copy_table_queries list and execute each index
+copy from s3 to staging tables '''
+
 
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
@@ -13,6 +16,10 @@ def load_staging_tables(cur, conn):
             conn.rollback()
 
 
+''' Loop inside the insert_table_queries list and execute each index
+insert data from staging tables to star schema '''
+
+
 def insert_tables(cur, conn):
     for query in insert_table_queries:
         try:
@@ -21,6 +28,15 @@ def insert_tables(cur, conn):
         except psycopg2.Error as errorMsg:
             print(errorMsg, "instert issue")
             conn.rollback()
+
+
+'''The main function will log to the star schema on Redshift(AWS) \
+    two functions:
+          * 1- load the data from stanging tables 
+          * 2- insert data into the star Schema  
+     including two arguments in each method:
+        * 1- connection to the database using the VPC
+        * 2- cursor to execute the SQL query '''
 
 
 def main():
